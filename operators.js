4f7da4b00c43b27a9ed2f12dc73fcce7900a51df
Rajesh <rajeshboddapati37@gmail.com>
2022-10-04 15:29:32 +0530

    04-10-2022_project1_commit1

---
+++ operators.js
@@ -0,0 +1,28 @@
+let a = 10;
+let b = 20;
+console.log("Initialized Values a=",a,"b=",b);
+console.log("Arithimatic operations:");
+console.log("a+b: ",a+b,"a-b:", a-b,"a*b:", a*b,"a/b:", a/b,"a%b:", a%b);
+console.log("-------------------------------------------------------------------------------");
+console.log("Relational operations: ");
+console.log("a>b, a<b, a<=b, a>=b  :",a>b, a<b, a<=b, a>=b);
+console.log("-------------------------------------------------------------------------------");
+console.log("Increment/decrement Operations: ");
+console.log("a++,b++,a--,b--  :",a++,b++,a--,b--);
+console.log("-------------------------------------------------------------------------------");
+console.log("Bitwise Operations: ");
+console.log("a & b, a|b, a^b, ~a, ~b, a<<b, a>>b, a>>>b   :",a & b, a|b, a^b, ~a, ~b, a<<b, a>>b, a>>>b);
+console.log("-------------------------------------------------------------------------------");
+console.log("Assignment Operations: ");
+let c=a;
+let d=b;
+console.log("c= ",c,"d= ",d);
+console.log("-------------------------------------------------------------------------------");
+console.log("Conditional Operations: ")
+console.log("a>b?a is greater':b is greater   :",a>b?"a is greater":"b is greater");
+console.log("-------------------------------------------------------------------------------");
+console.log("Special Operatons: ");
+console.log("Type of a is :",typeof(a));
+console.log("-------------------------------------------------------------------------------");
+console.log("Logical Operations: ");
+console.log("a && b, a || b, !a   :",a && b, a || b, !a);
